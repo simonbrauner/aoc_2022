9c51efcada0a4a578e6cdf8f3ee16c250a9bc4b5
Šimon Brauner <simonbrauner@seznam.cz>
2023-06-29 19:20:52 +0200

    05 part 1

--- 05.pl
+++ 05.pl
@@ -2,6 +2,8 @@
 
 use v5.36;
 
+use experimental qw{for_list};
+
 
 my @stacks;
 my @movements;
@@ -18,9 +20,26 @@ while (my $line = <>) {
     }
 }
 
+sub move_crates($stacks, @movements) {
+    foreach my $movement (@movements) {
+        my ($count, $source, $destination) = $movement->@*;
+
+        push $stacks->[$destination]->@*,
+            (pop $stacks->[$source]->@*) for (1..$count);
+    }
+}
+
+sub stack_tops($stacks) {
+    print pop $_->@* foreach grep { defined $_ } ($stacks->@*);
+    say '';
+}
+
 while (my $line = <>) {
     chomp $line;
 
     my @movement = $line =~ /^move (\d+) from (\d+) to (\d+)$/;
     push @movements, \@movement;
 }
+
+move_crates(\@stacks, @movements);
+stack_tops(\@stacks);
