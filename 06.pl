499e503b81100e31703ad7135886d1aaf6ea3dff
Šimon Brauner <simonbrauner@seznam.cz>
2023-06-29 22:35:24 +0200

    06 part 2

--- 06.pl
+++ 06.pl
@@ -5,19 +5,19 @@ use v5.36;
 use Set::Scalar;
 
 
-sub packet_marker_index($datastream) {
-    my @four = (split //, substr $datastream, 0, 4);
+sub packet_marker_index($datastream, $distinct_count) {
+    my @neighbors = (split //, substr $datastream, 0, $distinct_count);
 
-    for (my $index = 4; $index < length $datastream; $index++) {
-        return $index if Set::Scalar->new(@four)->size == 4;
+    for (my $index = $distinct_count; $index < length $datastream; $index++) {
+        return $index if Set::Scalar->new(@neighbors)->size == $distinct_count;
 
-        shift @four;
-        push @four, substr $datastream, $index, 1;
+        shift @neighbors;
+        push @neighbors, substr $datastream, $index, 1;
     }
 }
 
 my $datastream = <>;
 chomp $datastream;
 
-say packet_marker_index($datastream);
-
+say packet_marker_index($datastream, 4);
+say packet_marker_index($datastream, 14);
