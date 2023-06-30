16ffb460d8c875f9879bbd3b506f42ff0e793279
Šimon Brauner <simonbrauner@seznam.cz>
2023-06-30 21:15:06 +0200

    08 make edges visible

--- 08.pl
+++ 08.pl
@@ -2,6 +2,24 @@
 
 use v5.36;
 
+use List::Util qw{sum};
+
+
+sub make_edges_visible($forest) {
+    my $max_x = $forest->[0]->@* - 1;
+    my $max_y = $forest->@* - 1;
+
+    for (my $y = 0; $y <= $max_y; $y++) {
+        for (my $x = 0; $x <= $max_x; $x++) {
+            $forest->[$y][$x]{visible} = 1
+                if ($x % $max_x == 0 || $y % $max_y == 0);
+        }
+    }
+}
+
+sub count_visible($forest) {
+    sum map { $_->{visible} } map { $_->@* } $forest->@*
+}
 
 my @forest;
 
@@ -12,5 +30,6 @@ foreach my $line (<>) {
     push @forest, \@tree_line;
 }
 
-say "@forest";
-say $forest[0][0]{visible};
+make_edges_visible(\@forest);
+
+say count_visible(\@forest);
