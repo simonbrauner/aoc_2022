ed9a13dbfa8afe7294056b6008f435aebe4f8c50
Šimon Brauner <simonbrauner@seznam.cz>
2023-07-01 08:58:58 +0200

    08 part 1

--- 08.pl
+++ 08.pl
@@ -5,20 +5,55 @@ use v5.36;
 use List::Util qw{sum};
 
 
-sub make_edges_visible($forest) {
-    my $max_x = $forest->[0]->@* - 1;
-    my $max_y = $forest->@* - 1;
+sub transposed_forest(@forest) {
+    my @transposed;
 
-    for (my $y = 0; $y <= $max_y; $y++) {
-        for (my $x = 0; $x <= $max_x; $x++) {
-            $forest->[$y][$x]{visible} = 1
-                if ($x % $max_x == 0 || $y % $max_y == 0);
+    for my $index (0..@forest-1) {
+        my @tree_row = map { $_->[$index] } @forest;
+
+        push @transposed, \@tree_row;
+    }
+
+    return @transposed;
+}
+
+sub print_forest(@forest) {
+    foreach my $tree_row (@forest) {
+        print $_->{visible} foreach $tree_row->@*;
+        say '';
+    }
+}
+
+sub make_edges_visible(@forest) {
+    $_->[0]{visible} = 1 foreach @forest;
+}
+
+sub make_lines_visible(@forest) {
+    foreach my $tree_row (@forest) {
+        my $highest = $tree_row->[0];
+
+        foreach my $tree ($tree_row->@*) {
+            if ($tree->{height} > $highest->{height}) {
+                $tree->{visible} = 1;
+                $highest = $tree;
+            }
         }
     }
 }
 
-sub count_visible($forest) {
-    sum map { $_->{visible} } map { $_->@* } $forest->@*
+sub make_forest_visible(@forest) {
+    my @reversed = transposed_forest(reverse transposed_forest(@forest));
+    my @transposed = transposed_forest(@forest);
+    my @both = transposed_forest(reverse @forest);
+
+    foreach my $orientation (\@forest, \@reversed, \@transposed, \@both) {
+        make_edges_visible($orientation->@*);
+        make_lines_visible($orientation->@*);
+    }
+}
+
+sub count_visible(@forest) {
+    sum map { $_->{visible} } map { $_->@* } @forest
 }
 
 my @forest;
@@ -30,6 +65,6 @@ foreach my $line (<>) {
     push @forest, \@tree_line;
 }
 
-make_edges_visible(\@forest);
+make_forest_visible(@forest);
 
-say count_visible(\@forest);
+say count_visible(@forest);
