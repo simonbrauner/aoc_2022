5ea9b8ebe42f7b90628ac82b3c3f7b003825727d
Šimon Brauner <simonbrauner@seznam.cz>
2023-07-01 10:24:20 +0200

    08 part 2

--- 08.pl
+++ 08.pl
@@ -2,7 +2,9 @@
 
 use v5.36;
 
-use List::Util qw{sum};
+use experimental qw{for_list builtin};
+
+use List::Util qw{sum max product};
 
 
 sub transposed_forest(@forest) {
@@ -24,15 +26,19 @@ sub print_forest(@forest) {
     }
 }
 
-sub make_edges_visible(@forest) {
+sub compute_edges(@forest) {
     $_->[0]{visible} = 1 foreach @forest;
 }
 
-sub make_lines_visible(@forest) {
+sub compute_lines(@forest) {
     foreach my $tree_row (@forest) {
+        my @indices = map { 0 } (0..9);
         my $highest = $tree_row->[0];
 
-        foreach my $tree ($tree_row->@*) {
+        foreach my ($index, $tree) (builtin::indexed $tree_row->@*) {
+            push $tree->{distances}->@*, $index - max @indices[$tree->{height}..9];
+            $indices[$tree->{height}] = $index;
+
             if ($tree->{height} > $highest->{height}) {
                 $tree->{visible} = 1;
                 $highest = $tree;
@@ -41,14 +47,14 @@ sub make_lines_visible(@forest) {
     }
 }
 
-sub make_forest_visible(@forest) {
+sub compute_forest(@forest) {
     my @reversed = transposed_forest(reverse transposed_forest(@forest));
     my @transposed = transposed_forest(@forest);
     my @both = transposed_forest(reverse @forest);
 
     foreach my $orientation (\@forest, \@reversed, \@transposed, \@both) {
-        make_edges_visible($orientation->@*);
-        make_lines_visible($orientation->@*);
+        compute_edges($orientation->@*);
+        compute_lines($orientation->@*);
     }
 }
 
@@ -56,15 +62,21 @@ sub count_visible(@forest) {
     sum map { $_->{visible} } map { $_->@* } @forest
 }
 
+sub maximal_distances(@forest) {
+    max map { product $_->{distances}->@* } map { $_->@* } @forest
+}
+
 my @forest;
 
 foreach my $line (<>) {
     chomp $line;
 
-    my @tree_line = map { { height => $_, visible => 0 } } split //, $line;
+    my @tree_line = map { { height => $_, visible => 0,
+                            distances => [] } } split //, $line;
     push @forest, \@tree_line;
 }
 
-make_forest_visible(@forest);
+compute_forest(@forest);
 
 say count_visible(@forest);
+say maximal_distances(@forest);
