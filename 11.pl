7a73eb1f3497c4f5d25c2e378f7a4ba362349b9b
Šimon Brauner <simonbrauner@seznam.cz>
2023-07-10 11:06:20 +0200

    11 new monkey

--- 11.pl
+++ 11.pl
@@ -2,13 +2,34 @@
 
 use v5.36;
 
+use List::MoreUtils qw{mesh};
+
+
+sub create_operation($operation) {
+    my @keys = qw{left operator right};
+    my @values = split ' ', $operation;
+
+    my @result = mesh @keys, @values;
+    return \@result;
+}
+
+sub Monkey::new($class, $items, $operation, $test, $true, $false) {
+    my @items = split ', ', $items;
+
+    return bless {
+        items => \@items, operation => create_operation($operation),
+        divisible_by => $test, true => $true, false => $false,
+    }, $class;
+}
+
+my @monkeys;
 
 my $file_content = join '', <>;
-while ($file_content =~ /Monkey\s(\d+):\s+
+while ($file_content =~ /Monkey\s\d+:\s+
                         Starting\sitems:\s(.*)\s+
                         Operation:\snew\s=\s(.*)\s+
                         Test:\sdivisible\sby\s(\d+)\s+
                         If\strue:\sthrow\sto\smonkey\s(\d+)\s+
                         If\sfalse:\sthrow\sto\smonkey\s(\d+)/gx) {
-    say foreach @{^CAPTURE};
+    push @monkeys, Monkey->new(@{^CAPTURE});
 }
