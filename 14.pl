d98e1ac862c69ee5f925cb412656cc24d1290b4c
Šimon Brauner <simonbrauner@seznam.cz>
2023-07-26 16:24:04 +0200

    14 part 2

--- 14.pl
+++ 14.pl
@@ -6,18 +6,21 @@ use List::Util qw{min max};
 
 
 sub add_air($map) {
-    my @x_vals = map { (split ',', $_)[0] } keys $map->%*;
-    my @y_vals = map { (split ',', $_)[1] } keys $map->%*;
+    my $max_y = $map->@*+1;
 
-    for my $x ((min @x_vals)..(max @x_vals)) {
-        for my $y ((min @y_vals)..(max @y_vals)) {
-            $map->{"$x,$y"} //= '.';
+    for my $y (0..$max_y) {
+        for my $x (500-$y..500+$y) {
+            if ($y == $max_y) {
+                $map->[$y]{$x} = '#';
+            } else {
+                $map->[$y]{$x} //= '.';
+            }
         }
     }
 }
 
 sub create_map(@rocks) {
-    my %map = ('500,0' => '+');
+    my @map = ({ 500 => '+' });
 
     foreach my $rock (@rocks) {
         my ($x, $y) = split ',', $rock->[0];
@@ -29,37 +32,42 @@ sub create_map(@rocks) {
                 $x += $x < $new_x ? 1 : -1 if $x != $new_x;
                 $y += $y < $new_y ? 1 : -1 if $y != $new_y;
 
-                $map{"$x,$y"} = '#';
+                $map[$y]->{$x} = '#';
             } while $x != $new_x || $y != $new_y;
         }
     }
 
-    add_air(\%map);
+    add_air(\@map);
 
-    return %map;
+    return @map;
 }
 
-sub fall_sand($map) {
+sub fall_sand($map, $limit) {
     my ($x, $y) = (500, 0);
 
   OUTER: while (1) {
       $y++;
+      return 0 if $y > $limit;
 
       foreach my $dx (0, -1, 2) {
           $x += $dx;
 
-          return if !exists $map->{"$x,$y"};
-          next OUTER if ($map->{"$x,$y"} eq '.');
+          next OUTER if ($map->[$y]{$x} eq '.');
       }
 
-      return $map->{($x - 1) . ',' . ($y - 1)} = 'o';
+      if ($map->[$y - 1]{$x - 1} ne 'o') {
+          $map->[$y - 1]{$x - 1} = 'o';
+          return 1;
+      }
+
+      return 0;
   }
 }
 
-sub come_to_rest(%map) {
+sub come_to_rest($map, $limit = $map->@*) {
     my $counter = 0;
 
-    $counter++ while fall_sand(\%map);
+    $counter++ while fall_sand($map, $limit);
 
     return $counter;
 }
@@ -73,6 +81,8 @@ foreach my $line (<>) {
     push @rocks, \@rock;
 }
 
-my %map = create_map(@rocks);
+my @map = create_map(@rocks);
 
-say come_to_rest(%map);
+say my $on_rock = come_to_rest(\@map, @map-2);
+say $on_rock + come_to_rest(\@map);
+
