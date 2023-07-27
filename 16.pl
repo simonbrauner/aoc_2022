bfd50c2579e6ce216ef198ffc69eb6f2e49c0f64
Šimon Brauner <simonbrauner@seznam.cz>
2023-07-27 14:30:06 +0200

    16 create graph

--- 16.pl
+++ 16.pl
@@ -3,6 +3,30 @@
 use v5.36;
 
 
+sub create_layered_graph($vertices) {
+    my $graph = [];
+
+    foreach my $vertex (keys $vertices->%*) {
+        for my $layer (0..30) {
+            $graph->[$layer]{$vertex} = { name => $vertex };
+
+            if ($layer - 1 >= 0) {
+                push $graph->[$layer]{$vertex}{neighbors}->@*,
+                    { $graph->[$layer - 1]{$_} => 0 }
+                    foreach $vertices->{$vertex}{tunnels}->@*;
+            }
+
+            if ($layer - 2 >= 0) {
+                push $graph->[$layer]{$vertex}{neighbors}->@*,
+                    { $graph->[$layer - 2]{$_} => -$vertices->{$vertex}{flow_rate} * ($layer - 2) }
+                    foreach $vertices->{$vertex}{tunnels}->@*;
+            }
+        }
+    }
+
+    return $graph;
+}
+
 my $cave = {};
 
 my $file_content = join '', <>;
@@ -11,3 +35,5 @@ while ($file_content =~ /^Valve\ ([A-Z]+)\ has\ flow\ rate=(\d+);
     my @tunnels = split ', ', $3;
     $cave->{$1} = { flow_rate => $2, tunnels => \@tunnels };
 }
+
+my $graph = create_layered_graph($cave);
