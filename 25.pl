501c7eab15bac65647f312cf9db56d9ad55fe1f2
Šimon Brauner <simonbrauner@seznam.cz>
2023-08-02 15:23:37 +0200

    25 part 1

--- 25.pl
+++ 25.pl
@@ -6,12 +6,33 @@ use List::Util qw{sum};
 
 
 package SNAFU {
+    use List::Util qw{max};
+
+    my %to_normal = ('=' => -2, '-' => -1, 0 => 0, 1 => 1, 2 => 2);
+    my %from_normal = (-2 => '=', -1 => '-', 0 => 0, 1 => 1, 2 => 2);
+
     sub new($class, $number) {
         bless \$number, $class;
     }
 
     sub _plus($lhs, $rhs, $,) {
-        return SNAFU->new('----');
+        my $number = '';
+        my $current = 0;
+        my @left = reverse split //, $lhs->$*;
+        my @right = reverse split //, $rhs->$*;
+
+        for my $index (0..max(scalar @left, scalar@right)-1) {
+            $current += $to_normal{$_} foreach (($left[$index] // 0), ($right[$index] // 0));
+
+            my $sign = abs($current) > 2 ? ($current > 0 ? 1 : -1) : 0;
+
+            $number = $from_normal{$current - 5 * $sign} . $number;
+            $current = $sign;
+        }
+
+        $number = $current . $number if $current != 0;
+
+        return SNAFU->new($number);
     }
 
     sub _string($lhs, $, $,) {
