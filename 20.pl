524e956e295b5f08461a6ee1a6070b230937bff7
Šimon Brauner <simonbrauner@seznam.cz>
2023-08-02 14:06:27 +0200

    20 part 2

--- 20.pl
+++ 20.pl
@@ -6,27 +6,32 @@ use List::Util qw{sum};
 use List::MoreUtils qw{firstidx};
 
 
-sub mixed_numbers(@numbers) {
+sub mixed_numbers($times, @numbers) {
     my @original = @numbers;
 
-    foreach my $current (@original) {
-        my $index = firstidx { $_ eq $current } @numbers;
+    for (1..$times) {
+        foreach my $current (@original) {
+            my $index = firstidx { $_ eq $current } @numbers;
 
-        splice @numbers, $index, 1;
+            splice @numbers, $index, 1;
 
-        my $new_index = ($index + $current->$*) % @numbers;
+            my $new_index = ($index + $current->$*) % @numbers;
 
-        if ($new_index == 0) {
-            push @numbers, $current;
-        } else {
-            splice @numbers, $new_index, 0, $current;
+            if ($new_index == 0) {
+                push @numbers, $current;
+            } else {
+                splice @numbers, $new_index, 0, $current;
+            }
         }
     }
 
     return @numbers;
 }
 
-sub grove_coordinates(@numbers) {
+sub grove_coordinates($key, $times, @numbers) {
+    $_->$* *= $key foreach @numbers;
+    @numbers = mixed_numbers($times, @numbers);
+
     my $zero_index = firstidx { $_->$* eq 0 } @numbers;
 
     return sum map { $numbers[($_ + $zero_index) % @numbers]->$* } (1000, 2000, 3000);
@@ -40,5 +45,5 @@ foreach my $line (<>) {
     push @numbers, \$line;
 }
 
-my @mixed = mixed_numbers(@numbers);
-say grove_coordinates(@mixed);
+say grove_coordinates(1, 1, @numbers);
+say grove_coordinates(811589153, 10, @numbers);
