951b446cc6516086dfc26d3ca1c25310aef63434
Šimon Brauner <simonbrauner@seznam.cz>
2023-08-06 10:33:33 +0200

    21 depends on human

--- 21.pl
+++ 21.pl
@@ -13,11 +13,20 @@ sub eval_number($numbers, $monkey) {
     return $functions->{$expr[1]}->(map { eval_number($numbers, $_) } ($expr[0], $expr[2]));
 }
 
+sub depends_on_human($numbers, $monkey) {
+    my @expr = $numbers->{$monkey}->@*;
+    return $monkey eq 'humn' if @expr == 1;
+
+    return depends_on_human($numbers, $expr[0]) || depends_on_human($numbers, $expr[2]);
+}
+
 sub string_expr($numbers, $monkey) {
     my @expr = $numbers->{$monkey}->@*;
     return $expr[0] if @expr == 1;
 
-    my ($left, $right) = map { string_expr($numbers, $_) } ($expr[0], $expr[2]);
+    my ($left, $right) = map { depends_on_human($numbers, $_)
+                                   ? string_expr($numbers, $_)
+                                   : eval_number($numbers, $_) } ($expr[0], $expr[2]);
 
     return "($left $expr[1] $right)";
 }
