0f451bfc4600b751fb215fb62350c94b0851d6e2
Šimon Brauner <simonbrauner@seznam.cz>
2023-09-06 16:34:16 +0200

    22 hardcode wraps

--- 22.pl
+++ 22.pl
@@ -15,28 +15,21 @@ my $moves = { R => [1, 0], D => [0, 1], L => [-1, 0], U => [0, -1] };
 
 my $facing_number = { R => 0, D => 1, L => 2, U => 3 };
 
-sub get_coords($map, $x_y, $value = undef) {
-    my @result;
-    my $index = $x_y eq 'x' ? 0 : 1;
+sub wrap_2d($coords, $direction) {
+    my ($x, $y) = $coords->@*;
 
-    foreach my $position (keys $map->%*) {
-        my @coords = split ',', $position;
-        push @result, $coords[$index]
-            if !defined $value || $coords[($index+1) % 2] == $value;
-    }
-
-    return @result;
-}
-
-sub wrap_2d($coords, $direction, $map) {
     if ($direction eq 'R') {
-        $coords->[0] = min get_coords($map, 'x', $coords->[1]);
+        $coords->[0] = $y <= 100 ? 51 : 1;
     } elsif ($direction eq 'L') {
-        $coords->[0] = max get_coords($map, 'x', $coords->[1]);
+        $coords->[0] = do {
+            if ($y <= 50) { 150 } elsif ($y <= 150) { 100 } else { 50 }
+        }
     } elsif ($direction eq 'D') {
-        $coords->[1] = min get_coords($map, 'y', $coords->[0]);
+        $coords->[1] = $x <= 50 ? 101 : 1;
     } else {
-        $coords->[1] = max get_coords($map, 'y', $coords->[0]);
+        $coords->[1] = do {
+            if ($x <= 50) { 200 } elsif ($x <= 100) { 150 } else { 50 }
+        }
     }
 }
 
@@ -44,7 +37,7 @@ sub move($x_ref, $y_ref, $direction, $map, $wrap) {
     my $coords = [ map { $_->$* } ($x_ref, $y_ref) ];
     $coords->[$_] += $moves->{$direction}->[$_] for (0..1);
 
-    $wrap->($coords, $direction, $map)
+    $wrap->($coords, $direction)
         if !exists $map->{$coords->[0] . ',' . $coords->[1]};
 
     if ($map->{$coords->[0] . ',' . $coords->[1]} ne '#') {
@@ -54,8 +47,8 @@ sub move($x_ref, $y_ref, $direction, $map, $wrap) {
 }
 
 sub final_password($map, $path, $wrap) {
-    my $y = min get_coords($map, 'y');
-    my $x = min get_coords($map, 'x', $y);
+    my $y = 1;
+    my $x = 51;
     my $direction = 'R';
 
     while ($path =~ /((?:\d+)|(?:R|L))/g) {
