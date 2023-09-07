9a07379637be8fe35f60d90f5763df703c6f7759
Šimon Brauner <simonbrauner@seznam.cz>
2023-09-07 08:08:57 +0200

    22 wrap 3d

--- 22.pl
+++ 22.pl
@@ -33,6 +33,50 @@ sub wrap_2d($coords, $direction) {
     }
 }
 
+sub wrap_3d($coords, $direction) {
+    my ($x, $y) = $coords->@*;
+
+    $coords->@* = do {
+        if ($direction eq 'R') {
+            if ($y <= 50) {
+                (100, 151 - $y);
+            } elsif ($y <= 100) {
+                ($y + 50, 50);
+            } elsif ($y <= 150) {
+                (150, 151 - $y);
+            } else {
+                ($y - 100, 150);
+            }
+        } elsif ($direction eq 'L') {
+            if ($y <= 50) {
+                (1, 151 - $y);
+            } elsif ($y <= 100) {
+                ($y - 50, 101);
+            } elsif ($y <= 150) {
+                (51, 151 - $y);
+            } else {
+                ($y - 100, 1);
+            }
+        } elsif ($direction eq 'D') {
+            if ($x <= 50) {
+                ($x + 100, 1);
+            } elsif ($x <= 100) {
+                (50, $x + 100);
+            } else {
+                (100, $x - 50);
+            }
+        } else {
+            if ($x <= 50) {
+                (51, $x + 50);
+            } elsif ($x <= 100) {
+                (1, $x + 100);
+            } else {
+                ($x - 100, 200);
+            }
+        }
+    }
+}
+
 sub move($x_ref, $y_ref, $direction, $map, $wrap) {
     my $coords = [ map { $_->$* } ($x_ref, $y_ref) ];
     $coords->[$_] += $moves->{$direction}->[$_] for (0..1);
@@ -80,3 +124,4 @@ my $path = <>;
 chomp $path;
 
 say final_password($map, $path, \&wrap_2d);
+say final_password($map, $path, \&wrap_3d);
